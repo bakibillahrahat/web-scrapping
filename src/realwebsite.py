447024140f7f0421befe4593ed367c3c342beec9
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-10-17 22:33:11 +0600

    add published date

--- src/realwebsite.py
+++ src/realwebsite.py
@@ -4,7 +4,12 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_= 'joblist-comp-name').text.replace(' ', '')
-skills = job.find('span', class_ = 'srp-skills').text
-print(skills)
-print(company_name)
+company_name = job.find('h3', class_= 'joblist-comp-name').text.strip()
+skills = job.find('span', class_ = 'srp-skills').text.strip()
+published_date = job.find('span', class_ = 'sim-posted').text.strip()
+
+print(f'''
+    Company Name: {company_name}
+    Required Skills: {skills}
+    Published Date: {published_date}
+    ''')
