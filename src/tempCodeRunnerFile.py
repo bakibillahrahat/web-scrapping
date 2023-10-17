6c988cef146389e9b26acf6905b457f41a960afa
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-10-17 23:44:38 +0600

    fix some error

--- src/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-    company_name = job.find('h3', class_= 'joblist-comp-name').text.strip()
-    skills = job.find('span', class_ = 'srp-skills').text.strip()
-    published_date = job.find('span', class_ = 'sim-posted').text.strip()
